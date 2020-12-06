480e4e39d96d42e12506b3b67cca4ccc0b7a3112
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-12-06 11:23:41 +0000

    solution with stream

--- day6/part1.js
+++ day6/part1.js
@@ -1,13 +1,40 @@
+"use strict";
 const fs = require('fs');
+
+const stream = fs.createReadStream('./input.txt', { encoding: 'utf-8' });
+
+// 0.274ms
+console.time("STREAM:")
+const lineEndChar = '\n';
+stream.on('readable', () => {
+  let chunk;
+  let prevChunk;
+  let answers = 0;
+  const groupsAnswers = new Set();
+
+  while ((chunk = stream.read(1)) !== null) {
+    if (prevChunk === lineEndChar && chunk === lineEndChar) {
+      answers += groupsAnswers.size;
+      groupsAnswers.clear();
+    }
+
+    if(chunk !== lineEndChar) groupsAnswers.add(chunk)
+
+    prevChunk = chunk;
+  }
+
+  answers += groupsAnswers.size;
+  console.log("Answer: ", answers);
+  stream.destroy();
+})
+console.timeEnd("STREAM:")
+
+
 const data = fs.readFileSync('./input.txt', { encoding: 'utf-8' }).split('\n\n').map(i => i.split('\n'));
 
-// for (const groupanswers of data) {
-//   console.log(groupanswers)
-// }
-// console.log(data);
-
+// 3.318ms
+console.time("Loop:")
 const answer = data.reduce((acc,groupAnswers) => {
-  // console.log(groupAnswers);
   const set = new Set();
   groupAnswers.forEach(answers => {
     for (const a of answers) {
@@ -16,8 +43,8 @@ const answer = data.reduce((acc,groupAnswers) => {
       }
     }
   })
-  // console.log(set.size);
   return acc + set.size;;
 }, 0)
 
 console.log(answer);
+console.timeEnd("Loop:")
