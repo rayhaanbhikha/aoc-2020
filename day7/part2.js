dd61e1fa35e58c4b8bc946df5f43887511c0dd67
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-12-07 20:55:08 +0000

    refactored.

--- day7/part2.js
+++ day7/part2.js
@@ -1,12 +1,10 @@
 const fs = require('fs');
-
 const data = fs.readFileSync('./input.txt', { encoding: 'utf8' }).split('\n');
 
 const bags = {}
 const r = new RegExp(/(^[0-9]{1})\ ([a-z].*)/, 's');
 
 const queue = ['shiny gold'];
-
 do {
   const bagToFind = queue.shift();
   const reg = new RegExp(`^${bagToFind} `, 's');
@@ -30,14 +28,11 @@ do {
   }
 }while( queue.length !== 0 )
 
-// console.log(numberOfChildBags('vibrant plum'))
 console.log(bags)
 
 function computeNumOfBags(bags, value) {
   const res = bags[value];
-  if (res === 0) {
-    return 1
-  }
+  if (res === 0) return 1
 
   return res.reduce((acc, currentRes) => {
     const { quant, childBag } = currentRes;
@@ -48,9 +43,3 @@ function computeNumOfBags(bags, value) {
 }
 
 console.log(computeNumOfBags(bags, 'shiny gold'))
-
-
-
-
-
-// console.log(Object.keys(bagsFound).length - 1);
