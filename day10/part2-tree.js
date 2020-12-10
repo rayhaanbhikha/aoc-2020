8eff95114b1b79ab5a9c63a841f33f1f7cdbe426
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2020-12-10 16:15:57 +0000

    working example

--- day10/part2-tree.js
+++ day10/part2-tree.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const jolts = fs.readFileSync('./input.txt', { encoding: 'utf8' }).trim().split('\n').map(n => Number(n)).sort((a, b) => a - b);
+const jolts = fs.readFileSync('./input-example.txt', { encoding: 'utf8' }).trim().split('\n').map(n => Number(n)).sort((a, b) => a - b);
 
 const lastJolt = jolts[jolts.length - 1]+3;
 jolts.push(lastJolt);
@@ -35,55 +35,65 @@ class Node {
       }
     }
   }
+
+  addNode(newNode) {
+    if (!this.children.includes(newNode)) {
+      this.children.push(newNode);
+    }
+  }
+
+  print() {
+    console.log(this.value)
+    console.log("children: ")
+    this.children.forEach(c => console.log(c.value));
+    console.log("=================\n")
+    this.children.forEach(c => c.print());
+  }
 }
 
 const rootNode = new Node(0);
+nodeCache['0'] = rootNode;
 
 for (let i = 0; i < jolts.length; i++) {
   const jolt = jolts[i];
   rootNode.add(jolt);
 }
 
-console.log(nodeCache);
-
-const emptyNodes = Object.entries(nodeCache).filter(([key, node]) => node.children.length === 0 && node.value !== lastJolt).map(n => Number(n[0])).sort((a, b) => b - a);
+const emptyNodes = Object.entries(nodeCache).filter(([key, node]) => node.children.length < 3 && node.value !== lastJolt).map(n => Number(n[0])).sort((a, b) => b - a);
 
 console.log(emptyNodes);
 
 for (const emptyNodeValue of emptyNodes) {
   const node = getFromCache(emptyNodeValue);
-    const node1 = nodeCache[emptyNodeValue + 1];
-    const node2 = nodeCache[emptyNodeValue + 2];
-    const node3 = nodeCache[emptyNodeValue + 3];
+  const node1 = nodeCache[emptyNodeValue + 1];
+  const node2 = nodeCache[emptyNodeValue + 2];
+  const node3 = nodeCache[emptyNodeValue + 3];
 
   if (node1) {
-    node.children.push(node1)
+    node.addNode(node1)
   }
   if (node2) {
-    node.children.push(node2)
+    node.addNode(node2)
   }
   if (node3) {
-    node.children.push(node3)
+    node.addNode(node3)
   }
 }
 
 console.log(nodeCache);
+// rootNode.print();
 
 const queue = [rootNode];
+const reachedTarget = lastJolt;
+console.log(lastJolt)
+let reachedTargetNTimes = 0;
 do {
-  if(queue.length === 0) {queue.push(rootNode)}
   const currentNode = queue.shift();
-
-  console.log(currentNode.value);
-
-  if (currentNode.children.length === 0) {
-    // console.log("empty");
-    emptyFound = true;
-
-  } else {
-    queue.push(...currentNode.children);
+  if (currentNode.value === reachedTarget) {
+    reachedTargetNTimes++;
   }
+  queue.push(...currentNode.children);
+} while (queue.length !== 0);
 
-} while (queue.length );
-
-console.log(nodeCache)
+console.log(nodeCache);
+console.log(reachedTargetNTimes)
